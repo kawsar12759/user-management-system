3e2d699e37b1760fbe47fe16e458cad09f2c4ff9
kawsar12759 <kawsar15-12759@diu.edu.bd>
2024-09-16 12:28:58 +0600

    Made connection to MongoDB

---
+++ user-management-client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+
+
+const Navbar = () => {
+    return (
+        <div className="bg-green-400 p-5 text-4xl font-semibold">
+            <h1 className="text-center">User Management System</h1>
+        </div>
+    );
+};
+
+export default Navbar;
