3d752a80cc017edc9974767c1d244c498d1c86c9
kawsar12759 <kawsar15-12759@diu.edu.bd>
2024-09-16 16:27:43 +0600

    Worked on loading and deleting users

--- user-management-client/src/components/Home.jsx
+++ user-management-client/src/components/Home.jsx
@@ -1,10 +1,84 @@
+import Swal from 'sweetalert2'
+import { FaPencilAlt } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
 import { FaUser } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
+import { useState } from 'react';
 
 const Home = () => {
+    const loadedUsers = useLoaderData();
+    const [users,setUsers] = useState(loadedUsers);
+    let count = 1;
+    const handleDeleteUser = id => {
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/users/${id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "User has been deleted.",
+                                icon: "success"
+                            });
+                            const remaining = users.filter(user => user._id !== id);
+                            setUsers(remaining);
+                        }
+                    })
+
+            }
+        });
+    }
     return (
         <div className="py-28 px-56">
-            <Link to="/adduser"><button className="btn"><FaUser className="mr-1"/> Add User</button></Link>
+            <Link to="/adduser"><button className="btn"><FaUser className="mr-1" /> Add User</button></Link>
+
+            <div className="overflow-x-auto mt-14">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr className="bg-zinc-700 text-white text-center">
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>@Email</th>
+                            <th>Gender</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody >
+
+                        {
+                            users.map(user => (
+
+                                <tr key={user._id} className="text-center">
+                                    <th>{count++}</th>
+                                    <td >{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.gender}</td>
+                                    <td>{user.status}</td>
+                                    <td className="text-indigo-500"><button className="btn text-indigo-500 mr-5"><FaPencilAlt /></button>
+                                        <button className="btn text-indigo-500" onClick={() => handleDeleteUser(user._id)}><ImCross /></button>
+                                    </td>
+                                </tr>
+
+                            ))
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
